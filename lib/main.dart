0521e9a802e651f351661fa20d3aa3736a78ac96
NZ <nz@pop-oo.com>
2023-01-27 21:32:09 +0400

    feat: submit tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,35 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(
-//     const MaterialApp(
-//       home: Page1(),
-//     ),
-//   );
-// }
-
-// class Page1 extends StatelessWidget {
-//   const Page1({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () {
-//             Navigator.of(context).push(_createRoute());
-//           },
-//           child: const Text('Go!'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// Route _createRoute() {
-//   return PageRouteBuilder(
-//     pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
-//     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//       // const begin = Offset(0.0, 1.0);
-//       // const end = Offset.zero;
-//       // final tween = Tween(begin: begin, end: end);
-//       // final offsetAnimation = animation.drive(tween);
-
-//       // return SlideTransition(
-//       //   position: offsetAnimation,
-//       //   child: child,
-//       // );
-
-//       const begin = Offset(0.0, 1.0);
-//       const end = Offset.zero;
-//       const curve = Curves.ease;
-
-//       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-//       return SlideTransition(
-//         position: animation.drive(tween),
-//         child: child,
-//       );
-//     },
-//   );
-// }
-
-// class Page2 extends StatelessWidget {
-//   const Page2({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: const Center(
-//         child: Text('Page 2'),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: PhysicsCardDragDemo()));
+  runApp(const TabBarDemo());
 }
 
-class PhysicsCardDragDemo extends StatelessWidget {
-  const PhysicsCardDragDemo({super.key});
+class TabBarDemo extends StatelessWidget {
+  const TabBarDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const DraggableCard(
-        child: FlutterLogo(
-          size: 128,
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
         ),
-      ),
-    );
-  }
-}
-
-class DraggableCard extends StatefulWidget {
-  const DraggableCard({required this.child, super.key});
-
-  final Widget child;
-
-  @override
-  State<DraggableCard> createState() => _DraggableCardState();
-}
-
-class _DraggableCardState extends State<DraggableCard> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      child: Card(
-        child: widget.child,
       ),
     );
   }
